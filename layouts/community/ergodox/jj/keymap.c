6483ae124f91229546e452698635754f2bdcb2b5
Johan Ju <johanj93@gmail.com>
2019-04-21 10:36:21 +0200

    jj

--- layouts/community/ergodox/jj/keymap.c
+++ layouts/community/ergodox/jj/keymap.c
@@ -8,6 +8,7 @@
 #define GAME 1 // Gameing
 #define CTRL 3 // QWERTY Ctrl
 #define MDIA 4
+#define STAR 5
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_M,   KC_N,   KC_E,   KC_I,   KC_O,    NO_APOS,
         KC_VOLD,   KC_K,   KC_H,   KC_COMM,KC_DOT, NO_SLSH, KC_RSFT,
                            KC_LCTL,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
-        KC_NO,        KC_NO,
+        TO(STAR),        KC_NO,
         KC_PGUP,
         KC_PGDN,KC_BSPC,KC_ENT
     ),
@@ -61,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   !  |   @  |   {  |   }  |   |  |      |           | Next |   ^  |   =  |   *  |   +  |   ?  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   \  |------|           |------|   "  |MsLeft|MsDown| MsUp |MsRght|   PWR  |
+ * |        |   #  |   $  |   (  |   )  |   \  |------|           |------|   "  |MsLeft|MsDown| MsUp |MsRght|        |
  * |--------+------+------+------+------+------|      |           | Prev |------+------+------+------+------+--------|
- * |        |   <  |   %  |   [  |   ]  |   &  |      |           |      |   ~  |   Å  |   Ä  |   Ö  |      |  PrtSc |
+ * |        |   <  |   %  |   [  |   ]  |   &  |      |           |      |   ~  |   Å  |   Ä  |   Ö  |      | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       | Mclk |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
        KC_NO, KC_EXLM, NO_AT,   NO_LCBR, NO_RCBR, NO_PIPE, KC_NO,
        KC_NO, KC_HASH, NO_DLR,  NO_LPRN, NO_RPRN, NO_BSLS,
-       KC_NO, NO_LESS, KC_PERC, NO_LBRC, NO_RBRC, NO_AMPR, KC_NO,
+       KC_LSFT, NO_LESS, KC_PERC, NO_LBRC, NO_RBRC, NO_AMPR, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                                            KC_NO, KC_NO,
                                                     KC_NO,
@@ -90,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_NO, NO_CIRC, NO_EQL,  NO_ASTR, NO_PLUS, NO_QUES, KC_F12,
                 NO_QUO2, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
-       KC_NO, NO_TILD, NO_AA,   NO_AE,   NO_OSLH, KC_NO, KC_PSCR,
+       KC_NO, NO_TILD, NO_AA,   NO_AE,   NO_OSLH, KC_NO, KC_RSFT,
                          KC_NO,KC_NO,KC_NO,KC_NO, KC_NO,
        KC_NO, KC_NO,
        KC_NO,
@@ -102,8 +103,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_0,      KC_1,   KC_2,   KC_3,   KC_4,   KC_5,
         KC_I,     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
         KC_M,     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,
-        KC_H,     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_G,
-        KC_B,     KC_LCTL,   KC_LALT, KC_LALT,KC_LALT,
+        KC_B,     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_G,
+        KC_H,     KC_LCTL,   KC_LGUI, KC_LALT,KC_LALT,
                                                      KC_NO, TO(BASE),
                                                               KC_LOCK,
                                               KC_SPC,MO(SYMB), KC_NO,
@@ -115,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT, KC_NO,
         KC_NO,KC_NO,
         KC_NO,
-        KC_NO,KC_NO,KC_NO
+        KC_NO,KC_BSPC,KC_ENT
 ),
 
 [CTRL] = LAYOUT_ergodox(
@@ -159,6 +160,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_BTN3, KC_BTN1
 ),
 
+[STAR] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        KC_0,      KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
+        KC_TAB,    KC_Q,      KC_W,   KC_F,   KC_P,   KC_B,   KC_7,
+        KC_TAB,    KC_A,      KC_R,   KC_S,   KC_T,   KC_G,
+        KC_LSFT,   KC_Z,      KC_X,   KC_C,   KC_D,   KC_V,   KC_8,
+        KC_LCTL,KC_NO,KC_LGUI,KC_LALT,KC_LALT,
+                                                      KC_DEL, KC_ESC,
+                                                              KC_HOME,
+                                              KC_LCTL,KC_SPC,KC_END,
+        // right hand
+        KC_MUTE,   KC_8,   KC_0,   KC_2,   KC_4,   KC_6,    KC_F10,
+        KC_VOLU,   KC_J,   KC_L,   KC_U,   KC_Y,   NO_MINS, NO_ACUT,
+                   KC_M,   KC_N,   KC_E,   KC_I,   KC_O,    NO_APOS,
+        KC_VOLD,   KC_K,   KC_H,   KC_COMM,KC_DOT, NO_SLSH, KC_RSFT,
+                           KC_LCTL,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
+        TO(BASE),        KC_NO,
+        KC_PGUP,
+        KC_PGDN,KC_BSPC,KC_ENT
+),
+
 };
 
 
@@ -197,17 +219,21 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
+        case GAME:
             ergodox_right_led_1_on();
             break;
-        case 2:
+        case SYMB:
             ergodox_right_led_2_on();
             break;
-        case 3:
+        case CTRL:
             ergodox_right_led_1_on();
             ergodox_right_led_2_on();
             break;
-        case 4:
+        case MDIA:
+            ergodox_right_led_3_on();
+            break;
+        case STAR:
+            ergodox_right_led_1_on();
             ergodox_right_led_3_on();
             break;
         default:
