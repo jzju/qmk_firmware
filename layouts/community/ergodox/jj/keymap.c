959c0e3a57f8414911904369d3fac07c1d55a697
Johan Ju <johanj93@gmail.com>
2020-09-06 23:09:52 +0200

    gsym

--- layouts/community/ergodox/jj/keymap.c
+++ layouts/community/ergodox/jj/keymap.c
@@ -8,6 +8,7 @@
 #define GAME 1 // Gameing
 #define CTRL 3 // QWERTY Ctrl
 #define STAR 4
+#define GSYM 5
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,77 +37,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,      KC_7,      KC_5,   KC_3,   KC_1,   KC_9,   KC_J,
-        KC_TAB,      KC_Q,      KC_W,   KC_F,   KC_P,   KC_B,   KC_M,
+        KC_ESC,      KC_7,      KC_5,   KC_3,   KC_1,   KC_9,   KC_MPLY,
+        KC_TAB,      KC_Q,      KC_W,   KC_F,   KC_P,   KC_B,   KC_WAKE,
         KC_TAB,      KC_A,      KC_R,   KC_S,   KC_T,   KC_G,
-        KC_LSFT,     KC_Z,      KC_X,   KC_C,   KC_D,   KC_V,   KC_K,
+        KC_LSFT,     KC_Z,      KC_X,   KC_C,   KC_D,   KC_V,   KC_CAPS,
         LM(CTRL,MOD_LCTL),KC_NO,KC_LGUI,KC_LALT,KC_LALT,
-                                                      KC_DEL, TO(GAME),
-                                                              KC_HOME,
-                                              KC_SPC,MO(SYMB),KC_END,
+                                                      KC_NO, TO(GAME),
+                                                              KC_PGUP,
+                                              KC_SPC,MO(SYMB),KC_PGDN,
         // right hand
-        KC_MUTE,   KC_8,   KC_0,   KC_2,   KC_4,   KC_6,    KC_NO,
+        KC_MUTE,   KC_0,   KC_2,   KC_4,   KC_6,   KC_8,    KC_GRV,
         KC_VOLU,   KC_J,   KC_L,   KC_U,   KC_Y,   NO_MINS, NO_ACUT,
                    KC_M,   KC_N,   KC_E,   KC_I,   KC_O,    NO_APOS,
         KC_VOLD,   KC_K,   KC_H,   KC_COMM,KC_DOT, NO_SLSH, KC_RSFT,
-                           KC_LCTL,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
-        TO(STAR),        KC_NO,
-        KC_PGUP,
-        KC_PGDN,KC_BSPC,KC_ENT
+                           KC_LCTL,  KC_LALT,  KC_NO,  KC_NO,   KC_NO,
+        TO(STAR),        KC_DEL,
+        KC_HOME,
+        KC_END,KC_BSPC,KC_ENT
     ),
-/* Keymap 1: Symbol Layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | Play |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   %  |      |           | Next |   ^  |   =  |   *  |   +  |   ?  |   F12  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   &  |------|           |------|   "  |MsLeft|MsDown| MsUp |MsRght|  PrtSc |
- * |--------+------+------+------+------+------|      |           | Prev |------+------+------+------+------+--------|
- * |        |   <  |   >  |   [  |   ]  |   |  |      |           |      |   ~  |   Å  |   Ä  |   Ö  |   \  |  Sleep |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       | Mclk |      |      |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------| Rclk | Lclk |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// SYMBOLS
+
 [SYMB] = LAYOUT_ergodox(
        // left hand
        KC_NO, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
        KC_NO, KC_EXLM, NO_AT,   NO_LCBR, NO_RCBR, KC_PERC, KC_NO,
        KC_NO, KC_HASH, NO_DLR,  NO_LPRN, NO_RPRN, NO_AMPR,
-       KC_NO, NO_LESS, NO_GRTR, NO_LBRC, NO_RBRC, NO_PIPE, KC_NO,
+       KC_LSFT, NO_LESS, NO_GRTR, NO_LBRC, NO_RBRC, NO_PIPE, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                                            KC_NO, KC_NO,
-                                                    KC_NO,
+                                                  KC_NO,
                                   KC_NO, KC_NO, KC_NO,
        // right hand
        KC_MUTE,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_VOLU, NO_CIRC, NO_EQL,  NO_ASTR, NO_PLUS, NO_QUES, KC_F12,
                 NO_QUO2, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PSCR,
-       KC_VOLD, NO_TILD, NO_AA,   NO_AE,   NO_OSLH, NO_BSLS, KC_SLEP,
-                         KC_NO,KC_NO,KC_NO,KC_NO, KC_NO,
+       KC_VOLD, NO_TILD, NO_AA,   NO_AE,   NO_OSLH, NO_BSLS, KC_RSFT,
+                         KC_BTN2,KC_NO,KC_NO,KC_NO, KC_NO,
        KC_NO, KC_NO,
        KC_NO,
-       KC_NO, KC_BTN2, KC_BTN1
+       KC_NO, KC_BTN1, KC_BTN3
 ),
 
 [GAME] = LAYOUT_ergodox(
         // left hand
-        KC_ESC,   KC_0,      KC_1,   KC_2,   KC_3,   KC_4,   KC_5,
+        KC_ESC,   KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_I,     KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
         KC_M,     KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,
         KC_H,     KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_G,
-        KC_B,     KC_LCTL,   KC_LALT, KC_LALT,KC_LALT,
-                                                     KC_NO, TO(BASE),
-                                                              KC_LOCK,
-                                              KC_SPC,MO(SYMB), KC_NO,
+        KC_B,     KC_LCTL,   KC_NO, KC_LGUI,KC_LALT,
+                                                     KC_LOCK, TO(BASE),
+                                                               KC_PGUP,
+                                              KC_SPC,MO(GSYM), KC_PGDN,
         // right hand
         KC_NO, KC_6,   KC_7,  KC_8,   KC_9,   KC_0,        KC_NO,
         KC_NO, KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,        KC_NO,
@@ -158,6 +138,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(BASE),        KC_NO,
         KC_PGUP,
         KC_PGDN,KC_BSPC,KC_ENT
+),
+
+[GSYM] = LAYOUT_ergodox(
+       // left hand
+       KC_NO, KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_NO,
+       KC_NO, KC_EXLM, KC_P1, KC_P2, KC_P3, KC_NLCK, KC_NO,
+       KC_NO, KC_HASH, KC_P4, KC_P5, KC_P6, NO_AMPR,
+       KC_NO, NO_LESS, KC_P7, KC_P8, KC_P9, NO_PIPE, KC_NO,
+       KC_NO, KC_NO, KC_NO, KC_NO, KC_P0,
+                                           KC_NO, KC_NO,
+                                                    KC_NO,
+                                  KC_NO, KC_NO, KC_NO,
+       // right hand
+       KC_MUTE,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_VOLU, NO_CIRC, NO_EQL,  NO_ASTR, NO_PLUS, NO_QUES, KC_F12,
+                NO_QUO2, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PSCR,
+       KC_VOLD, NO_TILD, NO_AA,   NO_AE,   NO_OSLH, NO_BSLS, KC_SLEP,
+                         KC_NO,KC_NO,KC_NO,KC_NO, KC_NO,
+       KC_NO, KC_NO,
+       KC_NO,
+       KC_NO, KC_BTN2, KC_BTN1
 ),
 
 };
